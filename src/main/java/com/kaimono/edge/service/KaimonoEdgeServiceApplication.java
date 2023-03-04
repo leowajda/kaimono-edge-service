bb898760c8e5f9b701ffe4d5c28ddc688142e508
Leonardo Wajda <dev.leowajda@tuta.io>
2023-03-04 06:30:34 +0100

    :tada: Start kaimono-edge-service

---
+++ src/main/java/com/kaimono/edge/service/KaimonoEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kaimono.edge.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaimonoEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaimonoEdgeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaimono/edge/service/KaimonoEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaimono.edge.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaimonoEdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
